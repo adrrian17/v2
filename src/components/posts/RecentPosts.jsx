67c807ecc6888501156ed22cb42b6be07dcf0bad
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-02 02:44:03 -0600

    Add a component to display the recent posts

---
+++ src/components/posts/RecentPosts.jsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+
+export default function RecentPosts({ posts }) {
+  const titleColor = useColorModeValue('gray.800', 'brand.400');
+  const exceptColor = useColorModeValue('gray.500', 'gray.400');
+  const dateColor = useColorModeValue('brand.500', 'gray.200');
+
+  return (
+    <Container maxW={'container.md'}>
+      <Stack w={'fit-content'} mb={6}>
+        <Heading color={useColorModeValue('gray.800', 'white')}>
+          Recientes
+        </Heading>
+        <Box rounded={'full'} h={'4px'} bg={'brand.500'} w={'50%'} />
+      </Stack>
+      {posts.map((post) => {
+        const options = {
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        };
+
+        post.dateFormatted = new Intl.DateTimeFormat('es-MX', options).format(
+          new Date(post.published_at)
+        );
+
+        return (
+          <Stack key={post.id} my={12}>
+            <Link href={`/${post.slug}`} passHref>
+              <Text
+                fontSize={'xl'}
+                fontWeight={700}
+                color={titleColor}
+                cursor={'pointer'}
+              >
+                {post.title}
+              </Text>
+            </Link>
+            <Text py={2} fontSize={'lg'} color={exceptColor}>
+              {post.excerpt}
+            </Text>
+            <Stack direction={'row'}>
+              <Text color={dateColor} fontWeight={500}>
+                {post.dateFormatted}
+              </Text>
+              <Text color={dateColor} fontWeight={500}>
+                •
+              </Text>
+              <Text color={dateColor} fontWeight={500}>
+                {post.reading_time}{' '}
+                {post.reading_time > 1 ? 'minutos' : 'minuto'}
+              </Text>
+            </Stack>
+          </Stack>
+        );
+      })}
+    </Container>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
 import Head from 'next/head';
-import Intro from '~/components/general/Intro';
 
 import DefaultLayout from '~/layouts/DefaultLayout';
+import Intro from '~/components/general/Intro';
+import RecentPosts from '~/components/posts/RecentPosts';
 
-export default function Home() {
+import { getRecentPosts } from 'lib/ghost';
+
+export async function getStaticProps() {
+  const posts = await getRecentPosts();
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home({ posts }) {
   return (
     <>
       <Head>
         <title>Adrian (Sin Acento) Ayala</title>
-        <meta name="description" content="" />
+        <meta name="description" content="Hago café y guiones para cómic." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Intro />
+      <RecentPosts posts={posts} />
     </>
   );
 }
