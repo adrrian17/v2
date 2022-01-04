b8a012619daf4bc2c5425fdf9de091930810d166
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-04 17:32:32 -0600

    Remove the grid from the recent posts list

--- src/components/posts/RecentPosts.jsx
+++ src/components/posts/RecentPosts.jsx
@@ -17,43 +17,37 @@ export default function RecentPosts({ posts }) {
   const dateColor = useColorModeValue('gray.400', 'gray.200');
 
   return (
-    <Container maxW={'container.lg'} my={12}>
+    <Container maxW={'container.sm'} mx={'auto'} mb={12}>
       <Stack w={'fit-content'} mb={6}>
         <Heading color={useColorModeValue('gray.800', 'white')}>
           Recientes
         </Heading>
-        <Box rounded={'full'} h={'4px'} bg={'brand.500'} w={'50%'} />
+        <Box rounded={'full'} h={'3px'} bg={'brand.500'} w={'50%'} />
       </Stack>
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 3 }}
-        spacing={10}
-        alignContent={'center'}
-      >
-        {posts.map((post) => {
-          return (
-            <Stack key={post.id}>
-              <NextLink href={`/${post.slug}`} passHref>
-                <Link
-                  fontSize={'xl'}
-                  fontWeight={700}
-                  color={titleColor}
-                  cursor={'pointer'}
-                  _hover={{
-                    textDecoration: 'underline 2px',
-                  }}
-                >
-                  {post.frontmatter.title}
-                </Link>
-              </NextLink>
-              <Stack direction={'row'}>
-                <Text color={dateColor} fontWeight={500}>
-                  {dateFormatted(post.frontmatter.date)}
-                </Text>
-              </Stack>
+      {posts.map((post) => {
+        return (
+          <Stack key={post.id} my={6}>
+            <NextLink href={`/${post.slug}`} passHref>
+              <Link
+                fontSize={'xl'}
+                fontWeight={700}
+                color={titleColor}
+                cursor={'pointer'}
+                _hover={{
+                  textDecoration: 'underline 2px',
+                }}
+              >
+                {post.frontmatter.title}
+              </Link>
+            </NextLink>
+            <Stack direction={'row'}>
+              <Text color={dateColor} fontWeight={500}>
+                {dateFormatted(post.frontmatter.date)}
+              </Text>
             </Stack>
-          );
-        })}
-      </SimpleGrid>
+          </Stack>
+        );
+      })}
     </Container>
   );
 }
