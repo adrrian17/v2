bf8542d59ff91722ba1b3db43147c53f65029b29
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-04 21:21:47 -0600

    Make a change on how the date of the posts is displayed on the recents list

--- src/components/posts/RecentPosts.jsx
+++ src/components/posts/RecentPosts.jsx
@@ -3,9 +3,9 @@ import {
   Container,
   Heading,
   Link,
-  SimpleGrid,
   Stack,
   Text,
+  Flex,
   useColorModeValue,
 } from '@chakra-ui/react';
 
@@ -13,39 +13,48 @@ import NextLink from 'next/link';
 import { dateFormatted } from '~/utils/dateParser';
 
 export default function RecentPosts({ posts }) {
-  const titleColor = useColorModeValue('gray.800', 'brand.400');
-  const dateColor = useColorModeValue('gray.400', 'gray.200');
+  const titleColor = useColorModeValue('gray.800', 'whiteAlpha.900');
+  const dateColor = useColorModeValue('gray.500', 'gray.400');
 
   return (
     <Container maxW={'container.sm'} mx={'auto'} mb={12}>
       <Stack w={'fit-content'} mb={6}>
-        <Heading color={useColorModeValue('gray.800', 'white')}>
+        <Heading color={useColorModeValue('gray.800', 'whiteAlpha.900')}>
           Recientes
         </Heading>
         <Box rounded={'full'} h={'3px'} bg={'brand.500'} w={'50%'} />
       </Stack>
       {posts.map((post) => {
         return (
-          <Stack key={post.id} my={6}>
-            <NextLink href={`/${post.slug}`} passHref>
-              <Link
-                fontSize={'xl'}
-                fontWeight={700}
-                color={titleColor}
-                cursor={'pointer'}
-                _hover={{
-                  textDecoration: 'underline 2px',
-                }}
-              >
-                {post.frontmatter.title}
-              </Link>
-            </NextLink>
-            <Stack direction={'row'}>
-              <Text color={dateColor} fontWeight={500}>
+          <Flex
+            direction={{ base: 'column-reverse', md: 'row' }}
+            key={post.id}
+            my={4}
+          >
+            <Flex
+              flex={1}
+              justify={{ base: 'flex-start', md: 'flex-start' }}
+              align={'center'}
+            >
+              <Text color={dateColor}>
                 {dateFormatted(post.frontmatter.date)}
               </Text>
-            </Stack>
-          </Stack>
+            </Flex>
+            <Flex flex={2} ml={{ base: 0, md: -12 }}>
+              <NextLink href={`/${post.slug}`} passHref>
+                <Link
+                  fontSize={'xl'}
+                  fontWeight={700}
+                  color={titleColor}
+                  _hover={{
+                    textDecoration: 'underline 2px',
+                  }}
+                >
+                  {post.frontmatter.title}
+                </Link>
+              </NextLink>
+            </Flex>
+          </Flex>
         );
       })}
     </Container>
