66652e95a6475a5c6e281b9b8caab7e08db6a36d
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-04 22:55:39 -0600

    Align the copyright text and the rss icon on the footer

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -9,26 +9,19 @@ import { FiRss } from 'react-icons/fi';
 
 export default function Footer() {
   return (
-    <Container maxW={'container.sm'} mx={'auto'}>
-      <Stack
-        py={8}
-        direction={'row'}
-        justify={'space-between'}
-        align={'center'}
-      >
+    <Container maxW={'container.sm'}>
+      <Stack py={8} justify={'center'} align={'center'} spacing={6}>
         <Text color={useColorModeValue('gray.500', 'gray.400')}>
           © 2021 Adrian Ayala
         </Text>
-        <Stack direction={'row'} spacing={6}>
-          <Link
-            color={useColorModeValue('gray.500', 'gray.400')}
-            _hover={{ color: 'brand.500' }}
-            href="/rss.xml"
-            isExternal
-          >
-            <FiRss size={20} />
-          </Link>
-        </Stack>
+        <Link
+          color={useColorModeValue('gray.500', 'gray.400')}
+          _hover={{ color: 'brand.500' }}
+          href="/rss.xml"
+          isExternal
+        >
+          <FiRss size={20} />
+        </Link>
       </Stack>
     </Container>
   );
