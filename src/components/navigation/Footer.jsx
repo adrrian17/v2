0e4749da83801218da7c872e16d64cd15ab313f5
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-04 21:24:26 -0600

    Change some colors in the footer

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,4 +1,10 @@
-import { Container, Link, Stack, Text } from '@chakra-ui/react';
+import {
+  Container,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { FiRss } from 'react-icons/fi';
 
 export default function Footer() {
@@ -10,9 +16,16 @@ export default function Footer() {
         justify={'space-between'}
         align={'center'}
       >
-        <Text>© 2021 Adrian Ayala</Text>
+        <Text color={useColorModeValue('gray.500', 'gray.400')}>
+          © 2021 Adrian Ayala
+        </Text>
         <Stack direction={'row'} spacing={6}>
-          <Link _hover={{ color: 'brand.500' }} href="/rss" isExternal>
+          <Link
+            color={useColorModeValue('gray.500', 'gray.400')}
+            _hover={{ color: 'brand.500' }}
+            href="/rss"
+            isExternal
+          >
             <FiRss size={20} />
           </Link>
         </Stack>
