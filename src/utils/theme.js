3258d74ed6c183be5f5474e7dd375bd39495939d
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-04 21:17:02 -0600

    Update the dark mode body background color

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,6 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 const theme = extendTheme({
+  styles: {
+    global: (props) => ({
+      body: {
+        color: mode('gray.800', 'whiteAlpha.900')(props),
+        bg: mode('white', '#151719')(props),
+      },
+    }),
+  },
   fonts: {
     body: 'Work Sans',
   },
