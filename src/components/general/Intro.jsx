c93d093ed3537e2b3919b7d9f6e2d71c60bbb5f6
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-04 22:22:15 -0600

    Add more changes and improvements for the intro

--- src/components/general/Intro.jsx
+++ src/components/general/Intro.jsx
@@ -13,51 +13,47 @@ export default function Intro() {
     <Container
       maxW={'container.sm'}
       mx={'auto'}
-      mt={{ base: 0, md: 12 }}
-      mb={24}
+      mt={{ base: 0, md: 24 }}
+      mb={{ base: 16, md: 24 }}
     >
-      <Stack align={'center'} direction={{ base: 'column', md: 'row' }}>
-        <Stack flex={2} direction={'column'} pt={{ base: 12, md: 0 }}>
-          <Stack direction={'row'}>
-            <Text
-              color={useColorModeValue('gray.800', 'white')}
-              fontWeight={600}
-              fontSize={'5xl'}
-            >
-              Hola, soy Adrian
-            </Text>
-          </Stack>
-          <Text
-            letterSpacing={'tight'}
-            color={useColorModeValue('gray.500', 'gray.400')}
-            fontSize={'xl'}
-          >
-            Soy barista, escritor y programador. En mis ratos libres escribo
-            guiones para cómic. Ocasionalmente, hago proyectos de programación.
-          </Text>
-          <Stack pt={6} direction={'row'} spacing={6}>
-            <Link
-              _hover={{ color: 'brand.500' }}
-              href="https://twitter.com/adrrian17"
-              isExternal
-            >
-              <FiTwitter size={24} />
-            </Link>
-            <Link
-              _hover={{ color: 'brand.500' }}
-              href="https://github.com/adrrian17"
-              isExternal
-            >
-              <FiGithub size={24} />
-            </Link>
-            <Link
-              _hover={{ color: 'brand.500' }}
-              href="mailto:adrian.ayala17@gmail.com"
-            >
-              <FiMail size={24} />
-            </Link>
-          </Stack>
-        </Stack>
+      <Stack direction={'row'} my={6}>
+        <Text
+          color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+          fontWeight={700}
+          fontSize={'4xl'}
+        >
+          Hola, soy Adrian (Sin Acento)
+        </Text>
+      </Stack>
+      <Text
+        letterSpacing={'tight'}
+        color={useColorModeValue('gray.500', 'gray.400')}
+        fontSize={'xl'}
+      >
+        Soy barista, escritor y programador. En mis ratos libres escribo guiones
+        para cómic, ocasionalmente, hago proyectos de programación.
+      </Text>
+      <Stack pt={6} direction={'row'} spacing={6}>
+        <Link
+          _hover={{ color: 'brand.500' }}
+          href="https://twitter.com/adrrian17"
+          isExternal
+        >
+          <FiTwitter size={24} />
+        </Link>
+        <Link
+          _hover={{ color: 'brand.500' }}
+          href="https://github.com/adrrian17"
+          isExternal
+        >
+          <FiGithub size={24} />
+        </Link>
+        <Link
+          _hover={{ color: 'brand.500' }}
+          href="mailto:adrian.ayala17@gmail.com"
+        >
+          <FiMail size={24} />
+        </Link>
       </Stack>
     </Container>
   );
