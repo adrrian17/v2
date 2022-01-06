429c387203ed5c373f63bac62d3d4eb4fecc8cd7
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-06 16:02:57 -0600

    Make some improvements to the recent posts list

--- src/components/posts/RecentPosts.jsx
+++ src/components/posts/RecentPosts.jsx
@@ -7,26 +7,30 @@ import {
   Text,
   Flex,
   useColorModeValue,
+  Button,
+  IconButton,
 } from '@chakra-ui/react';
 
 import NextLink from 'next/link';
+import { FiArrowRightCircle } from 'react-icons/fi';
 import { dateFormatted } from '~/utils/dateParser';
 
 export default function RecentPosts({ posts }) {
   const titleColor = useColorModeValue('gray.800', 'whiteAlpha.900');
-  const dateColor = useColorModeValue('gray.500', 'gray.400');
+  const dateColor = useColorModeValue('gray.500', 'gray.500');
 
   return (
     <Container maxW={'container.sm'} mx={'auto'} mb={12}>
       <Stack w={'fit-content'} mb={6}>
-        <Heading
+        <Text
+          fontSize={'xl'}
           fontFamily={'Work Sans'}
           fontWeight={700}
           color={useColorModeValue('gray.800', 'whiteAlpha.900')}
         >
           Recientes ⏰
-        </Heading>
-        <Box rounded={'full'} h={'3px'} bg={'brand.500'} w={'50%'} />
+        </Text>
+        <Box rounded={'full'} h={'3px'} bg={'brand.400'} w={'50%'} />
       </Stack>
       {posts.map((post) => (
         <Flex
@@ -59,6 +63,21 @@ export default function RecentPosts({ posts }) {
           </Flex>
         </Flex>
       ))}
+      <Flex flex={1} justify={'center'} mt={10}>
+        <NextLink href="/archivo" passHref>
+          <Button
+            variant={'link'}
+            rightIcon={<FiArrowRightCircle />}
+            _focus={{ ring: 0 }}
+            _hover={{
+              textDecoration: 'none',
+              color: useColorModeValue('gray.800', 'whiteAlpha.900'),
+            }}
+          >
+            Ver más posts
+          </Button>
+        </NextLink>
+      </Flex>
     </Container>
   );
 }
