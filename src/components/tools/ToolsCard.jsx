4a25a077e1b47b17b09718208e4900d346674fe4
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-06 16:03:24 -0600

    Make some improvements to the tools card

--- src/components/tools/ToolsCard.jsx
+++ src/components/tools/ToolsCard.jsx
@@ -10,34 +10,42 @@ import {
 } from '@chakra-ui/react';
 
 import NextLink from 'next/link';
+import { FiChevronRight } from 'react-icons/fi';
 
 export default function ToolsCard() {
   return (
     <Container maxW={'container.sm'} mx={'auto'} mb={12}>
       <LinkBox>
         <Stack
-          p={8}
-          bg={useColorModeValue('gray.50', 'whiteAlpha.100')}
+          p={6}
+          direction={'row'}
+          align={'center'}
+          justify={'space-between'}
+          bg={useColorModeValue('gray.100', 'whiteAlpha.100')}
           borderRadius={'xl'}
           _hover={{
-            bg: useColorModeValue('gray.100', 'whiteAlpha.200'),
+            bg: useColorModeValue('gray.200', 'whiteAlpha.200'),
           }}
         >
-          <NextLink href="/herramientas" passHref>
-            <LinkOverlay>
-              <Stack w={'fit-content'} mb={6}>
-                <Heading
-                  fontFamily={'Work Sans'}
-                  fontWeight={700}
-                  color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                >
-                  Herramientas 🛠
-                </Heading>
-                <Box rounded={'full'} h={'3px'} bg={'brand.500'} w={'50%'} />
-              </Stack>
-            </LinkOverlay>
-          </NextLink>
-          <Text>Las apps y servicios que uso en el día a día.</Text>
+          <Stack>
+            <NextLink href="/herramientas" passHref>
+              <LinkOverlay>
+                <Stack w={'fit-content'} mb={2}>
+                  <Text
+                    fontSize={'xl'}
+                    fontFamily={'Work Sans'}
+                    fontWeight={700}
+                    color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                  >
+                    Herramientas 🛠
+                  </Text>
+                  <Box rounded={'full'} h={'3px'} bg={'brand.400'} w={'50%'} />
+                </Stack>
+              </LinkOverlay>
+            </NextLink>
+            <Text>Las apps y servicios que uso en el día a día.</Text>
+          </Stack>
+          <FiChevronRight size={30} />
         </Stack>
       </LinkBox>
     </Container>
