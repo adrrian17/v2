fb916e2f377d5cd8909a9d9534eb66dbe1c4ddf6
Adrian Ayala <adrian.ayala17@gmail.com>
2022-01-11 20:39:12 -0600

    Add an animation for the tools card

--- src/components/tools/ToolsCard.jsx
+++ src/components/tools/ToolsCard.jsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Container,
-  Heading,
   LinkBox,
   LinkOverlay,
   Stack,
@@ -9,45 +8,58 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-import NextLink from 'next/link';
+import { motion } from 'framer-motion';
 import { FiChevronRight } from 'react-icons/fi';
+
+import NextLink from 'next/link';
 
 export default function ToolsCard() {
   return (
     <Container maxW={'container.sm'} mx={'auto'} mb={12}>
-      <LinkBox>
-        <Stack
-          p={6}
-          direction={'row'}
-          align={'center'}
-          justify={'space-between'}
-          bg={useColorModeValue('gray.100', 'whiteAlpha.100')}
-          borderRadius={'xl'}
-          _hover={{
-            bg: useColorModeValue('gray.200', 'whiteAlpha.200'),
-          }}
-        >
-          <Stack>
-            <NextLink href="/herramientas" passHref>
-              <LinkOverlay>
-                <Stack w={'fit-content'} mb={2}>
-                  <Text
-                    fontSize={'xl'}
-                    fontFamily={'Work Sans'}
-                    fontWeight={700}
-                    color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                  >
-                    Herramientas 🛠
-                  </Text>
-                  <Box rounded={'full'} h={'3px'} bg={'brand.400'} w={'50%'} />
-                </Stack>
-              </LinkOverlay>
-            </NextLink>
-            <Text>Las apps y servicios que uso en el día a día.</Text>
+      <motion.div
+        whileHover={{
+          scale: 1.02,
+        }}
+      >
+        <LinkBox>
+          <Stack
+            p={6}
+            direction={'row'}
+            align={'center'}
+            justify={'space-between'}
+            bg={useColorModeValue('gray.100', 'whiteAlpha.100')}
+            borderRadius={'xl'}
+            _hover={{
+              bg: useColorModeValue('gray.200', 'whiteAlpha.200'),
+            }}
+          >
+            <Stack>
+              <NextLink href="/herramientas" passHref>
+                <LinkOverlay>
+                  <Stack w={'fit-content'} mb={2}>
+                    <Text
+                      fontSize={'xl'}
+                      fontFamily={'Work Sans'}
+                      fontWeight={700}
+                      color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                    >
+                      Herramientas 🛠
+                    </Text>
+                    <Box
+                      rounded={'full'}
+                      h={'3px'}
+                      bg={'brand.400'}
+                      w={'50%'}
+                    />
+                  </Stack>
+                </LinkOverlay>
+              </NextLink>
+              <Text>Las apps y servicios que uso en el día a día.</Text>
+            </Stack>
+            <FiChevronRight size={30} />
           </Stack>
-          <FiChevronRight size={30} />
-        </Stack>
-      </LinkBox>
+        </LinkBox>
+      </motion.div>
     </Container>
   );
 }
