e7662242a2ae9c31b78f7a03c8cea0d009f63bbc
Adrian Ayala <adrian.ayala17@gmail.com>
2022-03-24 14:07:59 -0600

    Remove the underline in the tools and scripts cards

--- src/components/scripts/ScriptsCard.jsx
+++ src/components/scripts/ScriptsCard.jsx
@@ -1,11 +1,11 @@
 import {
-  Box,
-  Container,
-  LinkBox,
-  LinkOverlay,
-  Stack,
-  Text,
-  useColorModeValue,
+	Box,
+	Container,
+	LinkBox,
+	LinkOverlay,
+	Stack,
+	Text,
+	useColorModeValue,
 } from '@chakra-ui/react';
 
 import { motion } from 'framer-motion';
@@ -14,48 +14,42 @@ import { FiChevronRight } from 'react-icons/fi';
 import NextLink from 'next/link';
 
 export default function ScriptsCard() {
-  return (
-    <Container maxW={'container.sm'} mx={'auto'}>
-      <motion.div
-        whileHover={{
-          scale: 1.05,
-        }}
-      >
-        <LinkBox>
-          <Stack
-            p={6}
-            direction={'row'}
-            align={'center'}
-            justify={'center'}
-            borderColor={'brand.400'}
-            borderWidth={3}
-            borderRadius={'xl'}
-          >
-            <Stack align={'center'}>
-              <NextLink href="/guiones" passHref>
-                <LinkOverlay>
-                  <Stack w={'fit-content'} mb={2}>
-                    <Text
-                      fontSize={'xl'}
-                      fontFamily={'Work Sans'}
-                      fontWeight={700}
-                      color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                    >
-                      Guiones 📝
-                    </Text>
-                    <Box
-                      rounded={'full'}
-                      h={'3px'}
-                      bg={'brand.400'}
-                      w={'50%'}
-                    />
-                  </Stack>
-                </LinkOverlay>
-              </NextLink>
-            </Stack>
-          </Stack>
-        </LinkBox>
-      </motion.div>
-    </Container>
-  );
+	return (
+		<Container maxW={'container.sm'} mx={'auto'}>
+			<motion.div
+				whileHover={{
+					scale: 1.05,
+				}}
+			>
+				<LinkBox>
+					<Stack
+						p={6}
+						direction={'row'}
+						align={'center'}
+						justify={'center'}
+						borderColor={'brand.400'}
+						borderWidth={3}
+						borderRadius={'xl'}
+					>
+						<Stack align={'center'}>
+							<NextLink href="/guiones" passHref>
+								<LinkOverlay>
+									<Stack w={'fit-content'}>
+										<Text
+											fontSize={'xl'}
+											fontFamily={'Work Sans'}
+											fontWeight={700}
+											color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+										>
+											Guiones 📝
+										</Text>
+									</Stack>
+								</LinkOverlay>
+							</NextLink>
+						</Stack>
+					</Stack>
+				</LinkBox>
+			</motion.div>
+		</Container>
+	);
 }
--- src/components/tools/ToolsCard.jsx
+++ src/components/tools/ToolsCard.jsx
@@ -1,11 +1,11 @@
 import {
-  Box,
-  Container,
-  LinkBox,
-  LinkOverlay,
-  Stack,
-  Text,
-  useColorModeValue,
+	Box,
+	Container,
+	LinkBox,
+	LinkOverlay,
+	Stack,
+	Text,
+	useColorModeValue,
 } from '@chakra-ui/react';
 
 import { motion } from 'framer-motion';
@@ -14,51 +14,42 @@ import { FiChevronRight } from 'react-icons/fi';
 import NextLink from 'next/link';
 
 export default function ToolsCard() {
-  return (
-    <Container maxW={'container.sm'} mx={'auto'} mb={{ base: 6, md: 0 }}>
-      <motion.div
-        whileHover={{
-          scale: 1.05,
-        }}
-      >
-        <LinkBox>
-          <Stack
-            p={6}
-            direction={'row'}
-            align={'center'}
-            justify={'center'}
-            borderColor={'brand.400'}
-            borderWidth={3}
-            borderRadius={'xl'}
-            // _hover={{
-            //   bg: useColorModeValue('gray.200', 'whiteAlpha.200'),
-            // }}
-          >
-            <Stack align={'center'}>
-              <NextLink href="/herramientas" passHref>
-                <LinkOverlay>
-                  <Stack w={'fit-content'} mb={2}>
-                    <Text
-                      fontSize={'xl'}
-                      fontFamily={'Work Sans'}
-                      fontWeight={700}
-                      color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                    >
-                      Herramientas 🛠
-                    </Text>
-                    <Box
-                      rounded={'full'}
-                      h={'3px'}
-                      bg={'brand.400'}
-                      w={'50%'}
-                    />
-                  </Stack>
-                </LinkOverlay>
-              </NextLink>
-            </Stack>
-          </Stack>
-        </LinkBox>
-      </motion.div>
-    </Container>
-  );
+	return (
+		<Container maxW={'container.sm'} mx={'auto'} mb={{ base: 6, md: 0 }}>
+			<motion.div
+				whileHover={{
+					scale: 1.05,
+				}}
+			>
+				<LinkBox>
+					<Stack
+						p={6}
+						direction={'row'}
+						align={'center'}
+						justify={'center'}
+						borderColor={'brand.400'}
+						borderWidth={3}
+						borderRadius={'xl'}
+					>
+						<Stack align={'center'}>
+							<NextLink href="/herramientas" passHref>
+								<LinkOverlay>
+									<Stack w={'fit-content'}>
+										<Text
+											fontSize={'xl'}
+											fontFamily={'Work Sans'}
+											fontWeight={700}
+											color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+										>
+											Herramientas 🛠
+										</Text>
+									</Stack>
+								</LinkOverlay>
+							</NextLink>
+						</Stack>
+					</Stack>
+				</LinkBox>
+			</motion.div>
+		</Container>
+	);
 }
